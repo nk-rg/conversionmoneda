9729cb88d8efabd4de11fde0e526a9bcd621e30f
dico <diegocc.prg@gmail.com>
2024-02-26 23:14:18 -0500

    programacion reactiva

--- src/main/java/com/bluefokus/conversionmoneda/repository/MonedaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bluefokus.conversionmoneda.repository;
-
-import com.bluefokus.conversionmoneda.entity.Moneda;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonedaRepository extends JpaRepository<Moneda, Integer> {
-}
